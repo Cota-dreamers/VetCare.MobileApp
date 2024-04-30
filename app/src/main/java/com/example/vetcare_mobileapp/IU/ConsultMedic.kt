408bc8324ed5b3ea3d0887e136ad8e8d3112991c
JesusLazoTapia <u202019038@upc.edu.pe>
2024-04-30 01:14:52 -0500

    update proyect mobile

---
+++ app/src/main/java/com/example/vetcare_mobileapp/IU/ConsultMedic.kt
@@ -0,0 +1,21 @@
+package com.example.vetcare_mobileapp.IU
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.vetcare_mobileapp.R
+
+class ConsultMedic : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_consult_medic)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
