1410b8c9fa6aae5b379c7e3e32d9fc875bd02a9e
JesusLazoTapia <u202019038@upc.edu.pe>
2024-04-29 23:53:13 -0500

    Entregable primer mobile

---
+++ app/src/main/java/com/example/vetcare_mobileapp/IU/Conf_Users.kt
@@ -0,0 +1,21 @@
+package com.example.vetcare_mobileapp.IU
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.vetcare_mobileapp.R
+
+class Conf_Users : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_emergency)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btVet)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/example/vetcare_mobileapp/IU/MainActivity.kt
+++ app/src/main/java/com/example/vetcare_mobileapp/IU/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.vetcare_mobileapp.IU
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,9 +22,12 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val btLeft = findViewById<ImageView>(com.example.vetcare_mobileapp.R.id.btLeft)
+        val btconfig = findViewById<ImageView>(com.example.vetcare_mobileapp.R.id.btconfig)
         val ivEmergency = findViewById<ImageView>(com.example.vetcare_mobileapp.R.id.ivEmergency)
 
-        ivEmergency.setOnClickListener { // Crear un Intent para iniciar EmergencyActivity
+        // Crear un Intent para iniciar EmergencyActivity
+        ivEmergency.setOnClickListener {
             val intent = Intent(
                 this@MainActivity,
                 EmergencyActivity::class.java
@@ -31,6 +36,16 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        btconfig.setOnClickListener{
+            val intent = Intent(
+                this@MainActivity,
+                Conf_Users::class.java
+            )
+        }
+
+        btLeft.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Estas en el Inicio!!", Toast.LENGTH_SHORT).show()
+        }
 
     }
 
