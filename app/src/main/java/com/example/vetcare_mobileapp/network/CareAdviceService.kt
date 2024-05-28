f37ff77ee8495d01a11d590ebe4d101a5b9c6059
Sebastian <u20191e575@upc.edu.pe>
2024-05-28 12:15:37 -0500

    feat(careAdvice): updating services.

--- app/src/main/java/com/example/vetcare_mobileapp/network/CareAdviceService.kt
+++ app/src/main/java/com/example/vetcare_mobileapp/network/CareAdviceService.kt
@@ -1,10 +1,11 @@
 package com.example.vetcare_mobileapp.network
 
 import com.example.vetcare_mobileapp.communication.ApiResponse
+import com.example.vetcare_mobileapp.communication.CareAdviceResponse
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface CareAdviceService {
-    @GET("careadvice")
-    fun getAllCareAdvices(): Call<ApiResponse>
+    @GET("api/v1/advices")
+    fun getAllCareAdvices(): Call<List<CareAdviceResponse>>
 }
