0da6bd87a95f8713d5843ebb812116b4af801643
Rodrigo-Hidalgo-Nicho <U202014694@upc.edu.pe>
2024-06-19 17:57:11 -0500

    feat: update_5 fixed

--- app/src/main/java/com/example/vetcare_mobileapp/activities/BookAppointmentActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.vetcare_mobileapp.activities
-
-import androidx.appcompat.app.AppCompatActivity
-
-class BookAppointmentActivity : AppCompatActivity() {
-    // Aquí deberías implementar el código de tu actividad
-    // como el manejo de vistas, interacciones, etc.
-}
