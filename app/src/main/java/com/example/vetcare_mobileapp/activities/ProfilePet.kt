109e805b417e85f40c5f4c05e70dbe04dcf9e6fb
Yorusito <sebastian1987102@gmail.com>
2024-06-24 18:57:40 -0500

    feat(pet-profile): petprofile updated

--- app/src/main/java/com/example/vetcare_mobileapp/activities/ProfilePet.kt
+++ app/src/main/java/com/example/vetcare_mobileapp/activities/ProfilePet.kt
@@ -1,19 +1,10 @@
 package com.example.vetcare_mobileapp.activities
 
-import android.annotation.SuppressLint
-import android.app.Activity
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.vetcare_mobileapp.R
 import com.example.vetcare_mobileapp.communication.PetResponse
 import com.example.vetcare_mobileapp.models.Pet
@@ -23,10 +14,6 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 
 class ProfilePet : AppCompatActivity() {
 
@@ -38,14 +25,16 @@ class ProfilePet : AppCompatActivity() {
         btnAddPet.setOnClickListener {
             val name = findViewById<EditText>(R.id.etName).text.toString()
             val breed = findViewById<EditText>(R.id.etBreed).text.toString()
-            val weight = findViewById<EditText>(R.id.etWeight).text.toString().toFloat()
+            val weight = findViewById<EditText>(R.id.etWeight).text.toString().toFloatOrNull()
             val photoUrl = findViewById<EditText>(R.id.etImageUrl).text.toString()
             val type = findViewById<EditText>(R.id.etType).text.toString()
             val color = findViewById<EditText>(R.id.etColor).text.toString()
+            val date = findViewById<EditText>(R.id.etDate).text.toString()
 
-            // Generar la fecha actual en el formato ISO 8601
-            val dateFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.getDefault())
-            val date = dateFormatter.format(Date())
+            if (weight == null) {
+                Toast.makeText(this@ProfilePet, "Peso no válido", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
 
             val newPet = Pet(
                 name = name,
@@ -57,7 +46,6 @@ class ProfilePet : AppCompatActivity() {
                 date = date
             )
             addPet(newPet)
-
         }
     }
 
@@ -75,12 +63,13 @@ class ProfilePet : AppCompatActivity() {
                     Toast.makeText(this@ProfilePet, "Mascota añadida exitosamente", Toast.LENGTH_SHORT).show()
                     finish()
                 } else {
-                    Toast.makeText(this@ProfilePet, "Error: ${response.code()}", Toast.LENGTH_SHORT).show()
+                    val errorMessage = response.errorBody()?.string()
+                    Toast.makeText(this@ProfilePet, "Error: ${response.code()} - $errorMessage", Toast.LENGTH_SHORT).show()
                 }
             }
 
             override fun onFailure(call: Call<PetResponse>, t: Throwable) {
-                Toast.makeText(this@ProfilePet, "Error: ${t.message}", Toast.LENGTH_SHORT). show()
+                Toast.makeText(this@ProfilePet, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
             }
         })
     }
--- app/src/main/java/com/example/vetcare_mobileapp/network/PetService.kt
+++ app/src/main/java/com/example/vetcare_mobileapp/network/PetService.kt
@@ -6,6 +6,7 @@ import com.example.vetcare_mobileapp.models.Pet
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface PetService {
@@ -13,6 +14,6 @@ interface PetService {
     fun getAllPets(): Call<List<PetResponse>>
 
     @POST("api/v1/pets")
+    @Headers("Content-Type: application/json", "accept: */*")
     fun addPet(@Body pet: Pet): Call<PetResponse>
-
 }
